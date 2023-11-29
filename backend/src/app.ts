8c8c95af6206834ec71a6126eb69113753f7a58b
Kreigh Hirschy <khirschy@kreigh8.com>
2023-11-28 22:13:36 -0500

    api ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,31 @@
 import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
+import morgan from 'morgan'
 import userRoutes from './routes/users'
+import createHttpError, { isHttpError } from 'http-errors'
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
 
 app.use((req, res, next) => {
-  next(Error('Endpoint not found'))
+  next(createHttpError(404, 'Endpoint not found'))
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error)
   let errorMessage = 'An unknown error occurred'
-  if (error instanceof Error) {
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status
     errorMessage = error.message
   }
-  return res.status(500).json({ error: errorMessage })
+  return res.status(statusCode).json({ error: errorMessage })
 })
 
 export default app
--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,11 +1,41 @@
 import { RequestHandler } from "express"
 import UserModel from '../models/User'
+import createHttpError from "http-errors"
 
 export const getUsers: RequestHandler = async (req, res, next) => {
   try {
-    const users = await UserModel.find().exec()
+    const users = await UserModel.find().select('-password').lean().exec()
     res.status(200).json(users)
   } catch (error) {
     next(error)
   }
+}
+
+interface CreateUserBody {
+  email: string,
+  username?: string,
+  password: string
+}
+
+export const createUser: RequestHandler<unknown, unknown, CreateUserBody, unknown> = async (req, res, next) => {
+  const { email, username, password } = req.body
+
+
+  try {
+
+    if (!email) {
+      throw createHttpError(400, 'New user must have email')
+    }
+    const users = await UserModel.create({
+      email,
+      username,
+      
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateNote: RequestHandler = async (req, res, next) => {
+
 }
