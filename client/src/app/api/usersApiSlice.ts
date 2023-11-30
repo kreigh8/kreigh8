3f231fcacf6f17f0702d61eb8e96a19d922c8e09
Kreigh Hirschy <khirschy@kreigh8.com>
2023-11-29 22:22:03 -0500

    store

--- client/src/app/api/usersApiSlice.ts
+++ client/src/app/api/usersApiSlice.ts
@@ -32,3 +32,17 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     })
   })
 })
+
+export const {
+  useGetUsersQuery
+} = usersApiSlice
+
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select()
+
+const selectUsersData = createSelector(selectUsersResult, usersResult => usersResult.data)
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  selectIds: selectUserIds
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export default store
