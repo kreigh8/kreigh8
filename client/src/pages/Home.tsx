8f84bd21040427708550ac10dd1f721f6f450689
Kreigh Hirschy <khirschy@kreigh8.com>
2024-01-03 19:51:26 -0500

    home nav

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,37 @@
+import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from "@mui/material"
+
 const Home = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <AppBar color='transparent' sx={{ boxShadow: 'none' }} component="nav">
+        <Toolbar>
+          <Typography component='div' variant="h6" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            kreigh8
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Button sx={{ color: 'rgba(0, 0, 0, 0.87)' }}>
+              About
+            </Button>
+            <Button sx={{ color: 'rgba(0, 0, 0, 0.87)' }}>
+              Clients
+            </Button>
+            <Button sx={{ color: 'rgba(0, 0, 0, 0.87)' }}>
+              Contact
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component='main' sx={{ p: 3 }}>
+        <Toolbar />
+        <Container maxWidth="xl">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant='h1' textAlign='center'>Home</Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      
     </>
   )
 }
