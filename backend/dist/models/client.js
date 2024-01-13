b1b7ccf2dbb409081fb39e4f48b3642f89ea9e74
Kreigh Hirschy <khirschy@kreigh8.com>
2024-01-13 13:19:13 -0500

    build

---
+++ backend/dist/models/client.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const clientSchema = new mongoose_1.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    active: { type: Boolean }
+});
+exports.default = (0, mongoose_1.model)('Client', clientSchema);
