9404b18bbd21c8d8a7571e735a09e09e1c78903e
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-10 17:49:36 -0400

    link

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,9 +1,16 @@
+import { Cpu, House } from 'lucide-react'
 import Link from 'next/link'
 
 function AdminSidebar() {
   return (
-    <aside className="bg-accent hidden h-screen min-w-[15%] flex-col px-4 py-4 md:flex">
-      <Link href="/admin/technology">Technology</Link>
+    <aside className="bg-accent hidden h-screen min-w-[15%] flex-col gap-4 px-4 py-4 md:flex">
+      <Link href="/admin" className="flex items-center gap-2">
+        <House /> Home
+      </Link>
+      <Link href="/admin/technology" className="flex items-center gap-2">
+        <Cpu />
+        Technology
+      </Link>
     </aside>
   )
 }
