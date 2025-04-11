4894899bd265a7a7736aff8401bd17bd3cc70237
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-11 12:12:33 -0400

    next

--- src/app/(components)/Technologies.tsx
+++ src/app/(components)/Technologies.tsx
@@ -6,7 +6,7 @@ async function TechnologyList() {
   const technologies = (await getTechnologies()) as ITechnology[]
 
   return (
-    <section className="mt-4 grid gap-2 md:grid-cols-3 lg:grid-cols-4">
+    <section className="mt-4 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
       {technologies.map((technology) => (
         <TechCard key={technology._id} technology={technology} />
       ))}
