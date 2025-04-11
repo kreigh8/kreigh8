92729f414999427e4bb1805bcfe6b7d0243ed5dc
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-11 13:28:43 -0400

    link

--- src/app/(components)/TechCard.tsx
+++ src/app/(components)/TechCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card'
 
 import { ITechnology } from '@/model/Technology'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {
   technology: ITechnology
@@ -13,17 +14,19 @@ export function TechCard({ technology }: Props) {
   return (
     <Card className="justify-center">
       <CardContent>
-        <div className="flex items-center justify-center gap-4">
-          <Image
-            src={technology.imageUrl}
-            alt={technology.techName}
-            width={80}
-            height={80}
-            className="object-over"
-          />
+        <Link href={technology.techUrl} target="_blank">
+          <div className="flex items-center justify-center gap-4">
+            <Image
+              src={technology.imageUrl}
+              alt={technology.techName}
+              width={80}
+              height={80}
+              className="object-over"
+            />
 
-          <h3 className="text-bold text-xl">{technology.techName}</h3>
-        </div>
+            <h3 className="text-bold text-xl">{technology.techName}</h3>
+          </div>
+        </Link>
       </CardContent>
     </Card>
   )
