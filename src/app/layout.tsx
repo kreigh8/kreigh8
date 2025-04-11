c59bcfca4476a1e893de21f257f3600a3be4250f
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-11 19:46:01 -0400

    Add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -39,6 +40,8 @@ export default function RootLayout({
           >
             <Header />
             {children}
+
+            <Footer />
           </ThemeProvider>
         </body>
       </html>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-background @container overflow-hidden border-t px-4 py-4">
+      <div className="mx-auto flex items-center justify-center lg:max-w-[1400px]">
+        <p>Copyright &#169; {new Date().getFullYear()} kreigh8</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
