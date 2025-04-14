1f7dcfb79e3a4b12691c61146c0112a4ea0c07b4
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-14 09:41:25 -0400

    cursor on buttons; add contact me button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import { TechnologyForm } from '@/components/TechnologyForm'
 // import Image from 'next/image'
 
+import { Button } from '@/components/ui/button'
 import ClientList from './(components)/Clients'
 import TechnologyList from './(components)/Technologies'
 
@@ -8,8 +9,27 @@ export default function Home() {
   return (
     <div className="@container px-4 py-2">
       <main className="mx-auto lg:max-w-[1400px]">
-        <section className="grid gap-2 md:grid-cols-2">
-          <article>Left</article>
+        <section className="my-4 grid gap-2 md:grid-cols-2">
+          <article className="flex flex-col gap-4">
+            <h1 className="text-2xl md:text-3xl">Welcome to kreigh8!</h1>
+            <h3>
+              Hello! I&apos;m Kreigh Hirschy, a software engineer with over 10
+              years of experience based in Indianapolis, Indiana.
+            </h3>
+            <p>
+              I specialize in building clean and elegant web solutions using the
+              modern technologies. I am passionate about creating efficient and
+              scalable solutions that meet the needs of my clients.
+            </p>
+
+            <div className="flex justify-center gap-2">
+              <Button variant="secondary" className="w-fit">
+                Download Resume
+              </Button>
+
+              <Button className="w-fit">Contact Me</Button>
+            </div>
+          </article>
 
           <article>Right</article>
         </section>
