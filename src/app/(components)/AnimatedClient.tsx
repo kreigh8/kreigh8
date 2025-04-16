85cfb84759aae29416f292d890965fafa32c5e95
Kreigh Hirschy <khirschy@kreigh8.com>
2025-04-16 11:22:51 -0400

    Animated Client

---
+++ src/app/(components)/AnimatedClient.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { IClient } from '@/model/Client'
+import { motion } from 'motion/react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+function AnimatedClient({ client }: { client: IClient }) {
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hovered"
+      className="flex flex-col items-center justify-center"
+    >
+      <Link href={client.clientUrl} target="_blank" className="w-full">
+        <motion.div
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.25, delay: 0.025, ease: 'easeInOut' }
+          }}
+        >
+          <Image
+            src={client.imageUrl}
+            alt={client.clientName}
+            width={90}
+            height={60}
+            className="m-auto object-cover"
+          />
+        </motion.div>
+
+        <div className="relative block overflow-hidden text-center text-2xl whitespace-nowrap">
+          <div>
+            {client.clientName.split('').map((c, i) => {
+              return (
+                <motion.span
+                  variants={{ initial: { y: 0 }, hovered: { y: '-100%' } }}
+                  transition={{
+                    duration: 0.25,
+                    ease: 'easeInOut',
+                    delay: 0.025 * i
+                  }}
+                  className="inline-block"
+                  key={i}
+                >
+                  {c}
+                </motion.span>
+              )
+            })}
+          </div>
+          <div className="absolute inset-0">
+            {client.clientName.split('').map((c, i) => {
+              return (
+                <motion.span
+                  variants={{ initial: { y: '-100%' }, hovered: { y: 0 } }}
+                  transition={{
+                    duration: 0.25,
+                    ease: 'easeInOut',
+                    delay: 0.025 * i
+                  }}
+                  className="inline-block"
+                  key={i}
+                >
+                  {c}
+                </motion.span>
+              )
+            })}
+          </div>
+        </div>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default AnimatedClient
--- src/app/(components)/ClientCard.tsx
+++ src/app/(components)/ClientCard.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import { Card, CardContent } from '@/components/ui/card'
 import { IClient } from '@/model/Client'
-import Image from 'next/image'
-import Link from 'next/link'
+import AnimatedClient from './AnimatedClient'
 
 type Props = {
   client: IClient
@@ -13,21 +10,7 @@ export function ClientCard({ client }: Props) {
   return (
     <Card className="justify-center py-4">
       <CardContent>
-        <Link href={client.clientUrl} target="_blank">
-          <div className="flex items-center justify-center gap-4">
-            <Image
-              src={client.imageUrl}
-              alt={client.clientName}
-              width={60}
-              height={60}
-              className="object-over"
-            />
-
-            <h3 className="text-bold text-sm md:text-lg">
-              {client.clientName}
-            </h3>
-          </div>
-        </Link>
+        <AnimatedClient client={client} />
       </CardContent>
     </Card>
   )
