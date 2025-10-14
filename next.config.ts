73d5ae6579ebaa89adcd5f7b247aff9f5f5c143a
Kreigh Hirschy <khirschy@kreigh8.com>
2025-10-14 11:28:49 -0400

    Initial Convex, Clerk and NextJS setup

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['localhost', 'res.cloudinary.com']
-  },
-  env: {
-    NEXT_GOOGLE_CAPTHA_KEY: process.env.NEXT_GOOGLE_CAPTHA_KEY
-  }
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
