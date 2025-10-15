6bae49cfc6a8891ee12c93eaf4d09c479d2727ba
Kreigh Hirschy <khirschy@kreigh8.com>
2025-10-15 09:47:02 -0400

    Upload form

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -1,14 +1,17 @@
 import Clients from '@/components/admin/Clients'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
+import Link from 'next/link'
 
 export default function AdminClientPage() {
   return (
     <section className="flex flex-col gap-4">
       <div className="flex justify-between">
         <h1>Clients Page</h1>
-        <Button>
-          <Plus /> Add
+        <Button asChild>
+          <Link href="/admin/clients/new">
+            <Plus /> Add
+          </Link>
         </Button>
       </div>
 
