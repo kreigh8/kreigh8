2dbbe02d9fddfbe30b0d19e5d6566723c14afa20
Kreigh Hirschy <khirschy@kreigh8.com>
2025-10-25 16:39:47 -0400

    adjust layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Technology from '@/components/Technology'
 export default function Home() {
   return (
     <>
-      <main className="container mx-auto grid grid-cols-1 gap-4 p-4">
+      <main className="container mx-auto grid grid-cols-1 gap-4">
         <section className="grid grid-cols-1  md:grid-cols-2 gap-4 w-full">
           <article className="flex flex-col w-full gap-4">
             <h1 className="text-4xl font-bold">Welcome to kreigh8</h1>
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,12 +11,14 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-10">
-      <nav className="bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        <Link href="/">kreigh8</Link>
+      <nav className="bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800">
+        <div className="container flex justify-between mx-auto">
+          <Link href="/">kreigh8</Link>
 
-        <div className="flex gap-2">
-          <ThemeToggle />
-          <Login />
+          <div className="flex gap-2">
+            <ThemeToggle />
+            <Login />
+          </div>
         </div>
       </nav>
 
