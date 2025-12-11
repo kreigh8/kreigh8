8905dd0e9662bb59723cca92a5c41d1d8a3c2d63
Kreigh Hirschy <khirschy@kreigh8.com>
2025-12-10 22:05:53 -0500

    middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// const isProtectedRoute = createRouteMatcher(["/server"]);
-
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/'])
 
 export default clerkMiddleware(async (auth, req) => {
