1578f1d5b89977428c56da7f493e07c4169dcfe5
Kreigh Hirschy <khirschy@kreigh8.com>
2026-02-15 20:18:01 -0500

    cleanup

--- components/admin/Resume/Resume.tsx
+++ components/admin/Resume/Resume.tsx
@@ -1,15 +1,11 @@
 import ResumeForm from '@/components/forms/ResumeForm'
-import { api } from '@/convex/_generated/api'
-import { preloadQuery } from 'convex/nextjs'
 
 export default async function Resume() {
-  const preloadedResume = await preloadQuery(api.resume.getResume)
-
   return (
     <section className="flex flex-col gap-4">
       <h1>Resume</h1>
 
-      <ResumeForm preloadedResume={preloadedResume} />
+      <ResumeForm />
     </section>
   )
 }
