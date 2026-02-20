fb96289a0ae0ac80f00488525b9f4fb01eaf3c5c
Kreigh Hirschy <khirschy@kreigh8.com>
2026-02-20 17:55:07 -0500

    update image pattern

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://quick-sockeye-353.convex.cloud/**')]
+    remotePatterns: [
+      new URL('https://quick-sockeye-353.convex.cloud/**'),
+      new URL('https://clean-terrier-44.convex.cloud/**')
+    ]
   }
 }
 
